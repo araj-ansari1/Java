6a5dd9303964cbc5c355d8668dac56f708746f91
Araj Ansari <mohderajansari@gmail.com>
2025-11-28 00:38:20 +0530

    SecondActivity Class

---
+++ SecondActivity.java
@@ -0,0 +1,13 @@
+public class secondActivity
+{
+
+	public static void main(String args[])
+	{
+
+		System.out.println("Welcome to"+ args[0]);
+		System.out.println("Welcome to"+ args[1]);
+		System.out.println("Welcome to"+ args[2]);	
+	
+	}
+
+}
